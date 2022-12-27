34e6a2fa7b34d94d65fe62caab9c66fd0fdeff03
woojin <jwjley@naver.com>
2022-12-27 22:07:36 +0900

    2022-12-27 전우진 UI 수정 및 추가

--- app/src/main/java/kr/co/company/healthapplication/HomeActivity.java
+++ app/src/main/java/kr/co/company/healthapplication/HomeActivity.java
@@ -15,6 +15,6 @@ public class HomeActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_mypage,container,false);
+        return inflater.inflate(R.layout.activity_home,container,false);
     }
 }
