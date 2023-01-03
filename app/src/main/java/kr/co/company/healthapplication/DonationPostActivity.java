19e961027b703bfd5855724aade1c0da742c564d
woojin <jwjley@naver.com>
2023-01-03 18:46:12 +0900

    feat : 기부 캠페인 Activity 추가

---
+++ app/src/main/java/kr/co/company/healthapplication/DonationPostActivity.java
@@ -0,0 +1,14 @@
+package kr.co.company.healthapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonationPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_post);
+    }
+}
