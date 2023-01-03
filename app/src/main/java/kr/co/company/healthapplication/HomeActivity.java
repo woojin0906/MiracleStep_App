a567eb10a6c86824b3c96e12dd707cbc09fd086d
Lib0823 <robot082373@naver.com>
2023-01-03 22:46:41 +0900

    feat : ADD HomeActivity, RunningActivity (2023-01-03 이인범)

--- app/src/main/java/kr/co/company/healthapplication/HomeActivity.java
+++ app/src/main/java/kr/co/company/healthapplication/HomeActivity.java
@@ -5,16 +5,39 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class HomeActivity extends Fragment {
+
+    private AdView mAdView; // 구글 광고
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home,container,false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_home, container, false);
+
+        // 구글 광고API
+        MobileAds.initialize(getActivity(), new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+
+            }
+        });
+        mAdView = rootView.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        return rootView;
     }
 }
--- app/src/main/java/kr/co/company/healthapplication/RunningActivity.java
+++ app/src/main/java/kr/co/company/healthapplication/RunningActivity.java
@@ -1,20 +1,37 @@
 package kr.co.company.healthapplication;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RunningActivity extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+
+public class RunningActivity extends Fragment implements SensorEventListener{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_running,container,false);
+    }
+
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
