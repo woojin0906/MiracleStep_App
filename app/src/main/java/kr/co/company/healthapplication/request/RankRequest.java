2bc0afda75ba7a42cab0b7e7d79dd61c88922376
Lib0823 <robot082373@naver.com>
2023-01-10 02:20:24 +0900

    feat: 랭킹 화면 DB연결 완료.(2023-01-10 인범)

---
+++ app/src/main/java/kr/co/company/healthapplication/request/RankRequest.java
@@ -0,0 +1,35 @@
+package kr.co.company.healthapplication.request;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Rank값 서버로 전송 (2023-01-10 인범 수정)
+public class RankRequest extends StringRequest {
+    final static private String URL = "http://miraclestep.ivyro.net/Rank.php"; // 서버 URL 설정 (PHP 파일 연동.)
+    private Map<String, String> map;
+
+    public RankRequest(String category, Response.Listener<String> listener) {
+        // post방식으로 listener를 서버에 전송.
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("category", category);
+
+        Log.d("정보", category);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+
+
+
+
+}
