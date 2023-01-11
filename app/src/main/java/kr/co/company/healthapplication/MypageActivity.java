c0fc646e2d4a0e6daf43a857999da80266fe9f11
LeeSooo <dltn990930@naver.com>
2023-01-11 12:55:55 +0900

    feat: ADD MyPage Front.(2023-01-11 이수)

--- app/src/main/java/kr/co/company/healthapplication/MypageActivity.java
+++ app/src/main/java/kr/co/company/healthapplication/MypageActivity.java
@@ -2,7 +2,6 @@ package kr.co.company.healthapplication;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
@@ -13,25 +12,99 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import kr.co.company.healthapplication.request.UserInfoSelectRequest;
 
 /* 신체정보 변경(키, 체중 등등), 회원정보 변경, 기부 신청내역 */
 
 public class MypageActivity extends Fragment {
+    private TextView tvUserSetting, tvUserID, tvUserName, tvUserDStep;
+    private TextView tvUserInfoSetting, tvUserHeight, tvUserWeight, tvUserAge, tvUserBMI;
+    private TextView tvUserDonation;
+    private Button btnDonationReceipts ,btnLogOut;
 
-    private Button btnLogOut;
     private SharedPreferences pref;
-    private SharedPreferences.Editor editor;
+    private SharedPreferences.Editor  editor;
+    private String userId;
+    private Boolean stateUserTable;
+
+    private String returnID;
+    private String returnUserName;
+    private int returnUserDStep;
+    private String returnUserImg;
+    private double height;
+    private double weight;
+    private String birth;
+    private int totalUserDonation;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_mypage,container,false);
-        btnLogOut = rootView.findViewById(R.id.btnLogOut);
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.activity_mypage,container,false);
+        tvUserSetting = view.findViewById(R.id.tvUserSetting);
+        tvUserID = view.findViewById(R.id.tvUserID);
+        tvUserName = view.findViewById(R.id.tvUserName);
+        tvUserDStep = view.findViewById(R.id.tvUserDStep);
+        tvUserHeight = view.findViewById(R.id.tvUserHeight);
+        tvUserWeight = view.findViewById(R.id.tvUserWeight);
+        tvUserSetting = view.findViewById(R.id.tvUserSetting);
+        tvUserAge = view.findViewById(R.id.tvUserAge);
+        tvUserBMI = view.findViewById(R.id.tvUserBMI);
+        tvUserDonation = view.findViewById(R.id.tvUserDonation);
+        btnDonationReceipts = view.findViewById(R.id.btnDonationReceipts);
+        btnLogOut = view.findViewById(R.id.btnLogOut);
+
+        // 이용자 정보 가져오기.
+        pref = getActivity().getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        editor = pref.edit();
+        userId = pref.getString("UserID", "_");
+
+        selectUserInfo();   // DB에서 정보를 가져오는 메소드.
+
+        tvUserID.setText(returnID);
+        tvUserName.setText(returnUserName);
+        tvUserDStep.setText(Integer.toString(returnUserDStep));
+        // 1. 이미지... (2023-01-10 이수)
+
+        tvUserHeight.setText(Double.toString(height));
+        tvUserWeight.setText(Double.toString(weight));
+        
+        // 2. 나이 변환해야 함. (2023-01-10 이수)
+        tvUserAge.setText(birth);
+        
+        // 3. BMI 계산해야 함. (2023-01-10 이수)
+        //tvUserBMI.setText();
+
+        tvUserDonation.setText(Integer.toString(totalUserDonation));
+
+        tvUserSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UserInfoSettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDonationReceipts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DonationReceiptsActivity.class);
+                startActivity(intent);
+            }
+        });
+
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pref = getActivity().getSharedPreferences("pref", Activity.MODE_PRIVATE);
                 editor = pref.edit();
                 editor.remove("UserID").apply();
                 Toast.makeText(getActivity().getApplicationContext(),"로그아웃에 성공하였습니다.", Toast.LENGTH_SHORT).show();
@@ -41,6 +114,63 @@ public class MypageActivity extends Fragment {
             }
         });
 
-        return rootView;
+        return view;
+    }
+
+    private void selectUserInfo() {
+        // 유저의 정보 가져오기. (2023-01-10 이수)
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    stateUserTable = jsonObject.getBoolean("success"); // php를 통해서 "success"를 전송받음.
+
+                    if (stateUserTable == true) {
+                        returnID = jsonObject.getString("userID");
+                        returnUserName = jsonObject.getString("userName");
+                        returnUserDStep = jsonObject.optInt("userDStep", 0);
+                        returnUserImg = jsonObject.getString("userImg");
+
+                        height = jsonObject.optDouble("height", 0.0);
+                        weight = jsonObject.optDouble("weight", 0.0);
+                        birth = jsonObject.getString("birth");
+
+                        totalUserDonation = jsonObject.optInt("totalUserDonation", 0);
+
+                    } else {
+                        Toast.makeText(getActivity(), "이용자 정보를 확인하지 못했습니다.", Toast.LENGTH_SHORT).show();
+                        returnID = "에러 아이디";
+                        returnUserName = "에러 이름";
+                        returnUserDStep = 0;
+                        returnUserImg = "img";
+
+                        height = 0.0;
+                        weight = 0.0;
+                        birth = "2000-01-01";
+
+                        totalUserDonation = 0;
+                    }
+
+                } catch(Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "에러가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                    returnID = "에러 아이디";
+                    returnUserName = "에러 이름";
+                    returnUserDStep = 0;
+                    returnUserImg = "img";
+
+                    height = 0.0;
+                    weight = 0.0;
+                    birth = "2000-01-01";
+
+                    totalUserDonation = 0;
+                }
+            }
+        };
+        // 서버로 Volley를 이용해서 요청을 함.
+        UserInfoSelectRequest userSelectRequest = new UserInfoSelectRequest(userId, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        queue.add(userSelectRequest);
     }
 }
---
+++ app/src/main/java/kr/co/company/healthapplication/request/UserInfoSelectRequest.java
@@ -0,0 +1,39 @@
+package kr.co.company.healthapplication.request;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserInfoSelectRequest extends StringRequest {
+    final static private String URL = "http://miraclestep.ivyro.net/SelectUserInfo.php"; // 서버 URL 설정 (PHP 파일 연동.)
+    private Map<String, String> map;
+
+    public UserInfoSelectRequest(String userID, Response.Listener<String> listener){
+        // post방식으로 listener를 서버에 전송.
+        super(Method.POST, URL, listener, null);
+
+        // 현재 날짜 구하기
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String nowDate = now.format(formatter);
+
+        map = new HashMap<>();
+        map.put("userID", userID);
+        map.put("userInfoDate", nowDate);
+        
+        Log.d("검색 아이디", userID);
+        Log.d("검색 날짜", nowDate);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
