68889cf878534e3bcfd3987a4182622a4c12e902
Lib0823 <robot082373@naver.com>
2023-01-11 11:18:11 +0900

    chore: 랭킹 화면 DB연결 수정 및 완성.(2023-01-10 인범)

--- app/src/main/java/kr/co/company/healthapplication/php/Rank.php
+++ app/src/main/java/kr/co/company/healthapplication/php/Rank.php
@@ -6,7 +6,7 @@
     */
 
 /* (1) 서버 DB에 연결.*/
-    $con = mysqli_connect("localhost", "miraclestep", "비밀번호", "miraclestep"); // mysql 연결, IP, 사용자명, 비밀번호, 데이터베이스
+    $con = mysqli_connect("localhost", "miraclestep", "비밀번호??????", "miraclestep"); // mysql 연결, IP, 사용자명, 비밀번호, 데이터베이스
     mysqli_query($con, 'SET NAMES utf8'); // 인코딩을 utf-8로 세팅. (한글 전송이 가능해짐.)
 
 /* (2) 앱에서 선택한 카테고리를 가져옴. */
@@ -15,7 +15,7 @@
 /* (3) 현재 DB에 저장된 사용자들의 id와 카테고리의 값을 검색함. */
     $statement = mysqli_prepare($con, "SELECT UserID, ".$_POST['category']." FROM Rank ORDER BY ".$_POST['category']." DESC");
 //     mysqli_stmt_bind_param($statement, 1, $_POST['category']);
-//     mysqli_stmt_bind_param($statement, 2, $_POST['category']); ㅅㅂ
+//     mysqli_stmt_bind_param($statement, 2, $_POST['category']); ㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂ ㅅㅂ ㅅ ㅂ
     mysqli_stmt_execute($statement);
 
 // /* (4) DB안에 해당 카테고리가 일치하는 Rank정보 가져오기 정보 가져오기. */
