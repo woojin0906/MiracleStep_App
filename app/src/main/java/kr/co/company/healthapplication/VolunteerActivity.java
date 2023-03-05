f1b50e9b4c23fe7ed65389c540a3966522b7e99d
woojin <jwjley@naver.com>
2023-03-05 16:29:44 +0900

    feat : 봉사활동 액티비티 추가 (2023-03-05 전우진)

--- app/src/main/java/kr/co/company/healthapplication/VolunteerActivity.java
+++ app/src/main/java/kr/co/company/healthapplication/VolunteerActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,30 +16,49 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class VolunteerActivity extends Fragment {
 
-    Spinner spinner;
+//    private RecyclerView recyclerView;                                  // 리사이클러뷰
+//    private DonationAdapter adapter;                               // 리사이클러뷰 어댑터
+//    private LinearLayoutManager linearLayoutManager;
+//    private ArrayList<DonationData> arrayList;
+    private Spinner spinner;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_volunteer, container, false);
+
+        // 스피너 설정
         spinner = rootView.findViewById(R.id.spinner);
 
-//        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
-//        //R.array.spinner_array는 정해놓은 값 / android.R.layout.simple_spinner_dropdown_item은 기본으로 제공해주는 형식입니다.
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter); //어댑터에 연결해줍니다.
+        ArrayAdapter arrAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        //R.array.spinner_array는 정해놓은 값 / android.R.layout.simple_spinner_dropdown_item은 기본으로 제공해주는 형식입니다.
+        arrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrAdapter); //어댑터에 연결해줍니다.
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            } //이 오버라이드 메소드에서 position은 몇번째 값이 클릭됬는지 알 수 있습니다.
+            //getItemAtPosition(position)를 통해서 해당 값을 받아올수있습니다.
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
 //
-//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//            } //이 오버라이드 메소드에서 position은 몇번째 값이 클릭됬는지 알 수 있습니다.
-//            //getItemAtPosition(position)를 통해서 해당 값을 받아올수있습니다.
+//        // List 설정
+//        recyclerView = (RecyclerView) rootView.findViewById(R.id.rvDonationList);
+//        linearLayoutManager = new LinearLayoutManager(getActivity());
+//        recyclerView.setLayoutManager(linearLayoutManager);
 //
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) { }
-//        });
-        return null;
+//        arrayList = new ArrayList<>();
+
+
+
+
+        return rootView;
     }
 }
